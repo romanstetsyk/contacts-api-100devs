da5c79c3a53a0cb9d15c3ae8d4de3fc381211faf
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-06-08 19:48:39 +0100

    Change array to json

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const app = express();
 
 const PORT = 8000;
 
-const contacts = [
-    {
+const contacts = {
+    'alice': {
         'name': 'Alice',
         'tel': '+1 (404) 1234-100'
     },
-    {
+    'bob': {
         'name': 'Bob',
         'tel': '+1 (404) 1234-101'
     },
-    {
+    'chad': {
         'name': 'Chad',
         'tel': '+1 (404) 1234-102'
     }
-]
+}
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
@@ -29,12 +29,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/:name', (request, response) => {
     const name = request.params.name;
-    for (const person of contacts) {
-        if (person.name.toLowerCase() === name.toLowerCase()) {
-            response.json(person);
-        }
-    }
-    response.json({'name': name, 'tel': 'undefined'});
+    response.json(contacts[name.toLowerCase()] || {'name': name, 'tel': 'undefined'});
 })
 
 app.listen(process.env.PORT || PORT, () => console.log(`Listening port ${PORT}...`));
